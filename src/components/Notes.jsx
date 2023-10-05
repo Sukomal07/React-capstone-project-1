0efd16691baadc4c6e828eb0be35405172e58fc0
Sukomal07 <hypersukomal@gmail.com>
2023-10-05 19:51:46 +0530

    add note to local storage

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,19 +1,49 @@
-function Notes({ isActive, text, onClick }) {
+import { useState, useEffect } from "react";
+
+function Notes() {
+    const [isActive, setIsActive] = useState(false);
+    const [noteText, setNoteText] = useState("");
+    const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || []);
+    useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes));
+    }, [notes]);
+
+    function handleClick() {
+        setIsActive(!isActive);
+        if (isActive && noteText.trim() !== "") {
+            setNotes([...notes, noteText]);
+            setNoteText("");
+        }
+    }
+
+    const handleNoteChange = (event) => {
+        setNoteText(event.target.value);
+    };
+
     return (
         <div className="notes">
-            <div>
+            <div className="heading">
                 <h1>All Notes</h1>
-                <span onClick={onClick}> + Add note</span>
+                <span onClick={handleClick}>Add note</span>
             </div>
-            {
-                isActive ? (
-                    <textarea name="note" id="note" autoFocus placeholder="Write here"></textarea>
-                ) : (
-                    <p>{text}</p>
-                )
-            }
+            {isActive ? (
+                <textarea
+                    name="note"
+                    id="note"
+                    autoFocus
+                    placeholder="Write here"
+                    value={noteText}
+                    onChange={handleNoteChange}
+                ></textarea>
+            ) : (
+                <div className="notesContainer">
+                    {notes.map((note, index) => (
+                        <p key={index}>{note}</p>
+                    ))}
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
-export default Notes
+export default Notes;
